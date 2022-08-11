d6fc0947db5af9650387465f8a856fd6e464a679
zach-laptop <zach.macdonald.021@gmail.com>
2022-08-11 12:46:04 -0400

    Move to netlify. Github pages sucks

--- nuxt.config.js
+++ nuxt.config.js
@@ -3,10 +3,6 @@ import { defineNuxtConfig } from 'nuxt'
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
 	ssr: false,
-	target: 'static',
-  	router: {
-    		base: '/GymTool/'
-  	},
 	modules: ['nuxt-ionic'],
 	css: [
 		'@ionic/core/css/core.css',
