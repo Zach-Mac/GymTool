a616d4e1ec74e5964ba1920e26c3c8422d06a249
Zach M <zach.macdonald.021@gmail.com>
2022-08-11 12:28:20 -0400

    Update nuxt.config.js

--- nuxt.config.js
+++ nuxt.config.js
@@ -3,6 +3,10 @@ import { defineNuxtConfig } from 'nuxt'
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
 	ssr: false,
+	target: 'static',
+  	router: {
+    		base: '/GymTool/'
+  	},
 	modules: ['nuxt-ionic'],
 	css: [
 		'@ionic/core/css/core.css',
